9fa0b575441bcd26d812a80baf9408a2dedc48e0
Ashley Yearicks <55463183+ayearicks@users.noreply.github.com>
2020-09-16 09:58:56 -0400

    Overhaul everything
    Deleted all unnecessary files. Finished index.html.
    
    -Still need to add screenshots of completed projects
    -Need to update hidden modals

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,53 +1,12 @@
-// Global Variables
-let home      = document.getElementById("homeLink"),
-    projects  = document.getElementById("projectLink"),
-    about     = document.getElementById("aboutLink"),
-    resume    = document.getElementById("resumeLink"),
-    contact   = document.getElementById("contactLink"),
-    viewContent   = document.getElementById("viewContent");
+// Scroll Function to move Navbar from top to left side
+window.onscroll = function() {
+  moveNav();
+};
 
-
-// Event Listeners for each navigation button
-home.addEventListener("click", function(){ pageChange(this.getAttribute("href")); });
-projects.addEventListener("click", function(){ pageChange(this.getAttribute("href")); });
-about.addEventListener("click", function(){ pageChange(this.getAttribute("href")); });
-resume.addEventListener("click", function(){ pageChange(this.getAttribute("href")); });
-contact.addEventListener("click", function(){ pageChange(this.getAttribute("href")); });
-
-// Update Div function
-function pageChange(tar) {
-  /*
-  if link clicked equals id of target
-  change display of current content
-  toggle display of target content
-  else
-  change display to match clicked target
-  */
- switch (tar) {
-   case ('#home'):
-     home.classList.toggle("d-none");
-     home.classList.add("d-flex");
-     break;
-   case ('#projects'):
-     projects.classList.toggle("d-none");
-     projects.classList.toggle("d-flex");
-     break;
-   case ('#about'):
-     document.getElementById("about").classList.toggle("d-none");
-     document.getElementById("about").classList.toggle("d-flex");
-     break;
-   case ('#resume'):
-     document.getElementById("resume").classList.toggle("d-none");
-     document.getElementById("resume").classList.toggle("d-flex");
-     break;
-   case ('#contact'):
-     document.getElementById("contact").classList.toggle("d-none");
-     document.getElementById("contact").classList.toggle("d-flex");
-     break;
- 
-   default:
-     break;
- }
-console.log(tar);
-
+function moveNav() {
+  if( document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    document.getElementById("mainNavbar").classList.add("scrolled");
+  } else {
+    document.getElementById("mainNavbar").classList.remove("scrolled");
+  }
 }
