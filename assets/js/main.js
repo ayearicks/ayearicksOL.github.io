1e5bb0341ae47fd6f40ed7ab9d38b6cb0bc275a5
Ashley Yearicks <55463183+ayearicks@users.noreply.github.com>
2020-09-17 07:48:32 -0400

    Scroll function and Mobile Nav
    Updated js scroll functions.
    Start working on alternative to 'Hamburger' navigation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,31 @@
-// Scroll Function to move Navbar from top to left side
-window.onscroll = function() {
-  moveNav();
-};
+$(document).ready(function(){
 
-function moveNav() {
-  if( document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-    document.getElementById("mainNavbar").classList.add("scrolled");
-  } else {
-    document.getElementById("mainNavbar").classList.remove("scrolled");
+  // Scroll Function to move Navbar
+    $(document).scroll(function(){
+      let $nav = $("#mainNavbar");
+      
+      // Move 768px+ Nav from Top to Left
+      if ($(window).width() >= 768) {
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());      
+      }
+
+    });
+
+  // Navigation background color toggle
+  $("#navbarToggler").on("click", function(){
+    grayBackgrounds();
+  });
+
+  // Close menu if item is clicked
+  $(".nav-link").on("click", function(){
+    $("#hamburgerNav").toggleClass("show");
+    grayBackgrounds();
+  });
+
+  // DRY function for Background color changes
+  function grayBackgrounds() {
+    $("#mainNavbar").toggleClass("grayBackground");
+    $("#hamburgerNav").toggleClass("grayBackground");
   }
-}
+
+});
